aa47892a915a4209f99a30b5d681a2017acda4af
hiki349 <vladyakima@gmail.com>
2024-03-22 20:40:16 +0200

    refactor main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,19 +3,13 @@ package main
 import (
 	"context"
 	"go-shop/configuration"
-	"go-shop/internal/api/gql/resolvers"
-	"go-shop/internal/api/gql/runtime"
+	"go-shop/internal/api/gql"
 	"go-shop/internal/api/rest"
 	"go-shop/internal/domain/services"
 	"go-shop/internal/pkg/logger"
 	"go-shop/internal/storage/db"
 	"go-shop/internal/storage/repo"
 	"log"
-
-	"net/http"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 func main() {
@@ -39,31 +33,5 @@ func main() {
 	usersService := services.NewUsersService(userssRepo)
 
 	go rest.MustStartRestServer(usersService, config.RestPort, clog)
-	mustStartGqlServer(productsService, cartsService, usersService, config.GqlPort)
-}
-
-func mustStartGqlServer(
-	productsService *services.ProductsService,
-	cartsService *services.CartsService,
-	usersService *services.UsersService,
-	port string,
-) {
-	srv := handler.NewDefaultServer(
-		runtime.NewExecutableSchema(
-			runtime.Config{
-				Resolvers: &resolvers.Resolver{
-					ProductsService: productsService,
-					CartsService:    cartsService,
-					UsersService:    usersService,
-				},
-			},
-		),
-	)
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	gql.MustStartGqlServer(productsService, cartsService, usersService, config.GqlPort)
 }
